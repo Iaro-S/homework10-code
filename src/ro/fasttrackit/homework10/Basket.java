c565927ec2c106ebd7cf9359796e4c7b9ae08429
Iaro <iaroslav.slovak@gmail.com>
2021-04-21 13:05:02 +0300

    Initial commit

--- src/ro/fasttrackit/homework10/Basket.java
+++ src/ro/fasttrackit/homework10/Basket.java
@@ -1,22 +1,30 @@
 package ro.fasttrackit.homework10;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Basket {
     public static void main(String[] args) {
-        System.out.println (fruits);
-        System.out.println (find ("pere"));
-        System.out.println (find ("cirese"));
+        System.out.println ("Basket content:" + fruits);
+        System.out.println ("Fruit found in basket: " + findFruits ("pere"));
+        System.out.println ("Fruit found in basket: " + findFruits ("cirese"));
 
-        fruits.add ("portocale");
-        System.out.println (fruits);
+        System.out.println ("\n" + fruits);
+        System.out.println ("Fruit is on position: " + position ("prune"));
+        System.out.println ("Fruit is on position: " + position ("cirese"));
 
-        System.out.println (position ("prune"));
-        System.out.println (position ("cirese"));
+        System.out.println ("\n" + "Fruit found an removed from basket: " + removeFruits ("pere"));
+        System.out.println ("Fruit found an removed from basket: " + removeFruits ("cirese"));
+        System.out.println ("Basket content after removal: " + fruits);
 
-        fruits.remove ("pere");
-        System.out.println (fruits);
+        addFruits ("banane");
+        System.out.println ("\n" + "Basket content after addition: " + fruits);
+        addFruits ("struguri");
+        addFruits ("banane");
+        System.out.println ("\n" + "Basket content" + fruits);
+        System.out.println ("Unique basket content" + distinct (fruits));
+
+        System.out.println ("\n" + "Basket contains " + countFruits () + " fruits.");
+        System.out.println ("\n" + "Basket contains " + customCountFruits () + " fruits.");
     }
 
     private static final List<String> fruits = new ArrayList<> (List.of ("mere",
@@ -28,16 +36,56 @@ public class Basket {
             "capsuni",
             "pere"));
 
-    public static int position(String fruit) {
-        return fruits.indexOf (fruit);
-    }
-
-    public static boolean find(String fruit) {
+    public static boolean findFruits(String fruit) {
         for (String myFruit : fruits) {
             if (fruit.equals (myFruit)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static boolean removeFruits(String fruit) {
+        if (findFruits (fruit)) {
+            fruits.remove (fruit);
+            return true;
+        }
+        return false;
+    }
+
+    public static int position(String fruit) {
+        //return fruits.indexOf (fruit);
+        for (String myFruit : fruits) {
+            if (fruit.equals (myFruit)) {
+                return fruits.indexOf (fruit);
+            }
+        }
+        return -1;
+    }
+
+    public static Set<String> distinct(Collection<String> fruits) {
+        Set<String> distinctBasket = new HashSet<> ();
+        for (String myFruit :
+                fruits) {
+            distinctBasket.add (myFruit);
+        }
+        return distinctBasket;
+    }
+
+    public static String addFruits(String fruit) {
+        fruits.add (fruit);
+        return String.valueOf (fruits);
+    }
+
+    public static int countFruits() {
+        return fruits.size ();
+    }
+
+    public static int customCountFruits() {
+        int count = 0;
+        for (String myFruit : fruits) {
+            count++;
+        }
+        return count;
     }
 }
